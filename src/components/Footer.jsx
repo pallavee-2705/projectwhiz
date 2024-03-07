a6a6c4c478976d7b978873d05a2d524dbfb2fd07
pallavee-2705 <pallaveebisoyee@gmail.com>
2024-03-07 01:06:49 -0500

    implemented the basic layout for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,79 @@
 import React from 'react';
 import { footerlogo } from '../assets';
-
+import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram} from "react-icons/fa";
 function Footer() {
   return (
-    <div className='bg-blue-950 text-slate-50'>
-      <div className='py-12 px-10 mt-20 flex justify-between items-center text-slate-50 w-full'>
+    <div className='bg-blue-950 text-slate-50 px-12'>
+      <div className='py-4 mt-20 flex justify-between items-center text-slate-50 w-full'>
             {/* Left side */}
             <div>
+              <a href="/" className="cursor-pointer">
                     <img src={footerlogo} alt="footerlogo" className='h-14' />
+              </a>
                 </div>
 
                 {/* Right side */}
-                <div className='flex font-light gap-12'>
+                <div className='flex font-light gap-8'>
                     {/* About */}
-                    <div>About</div>
+                    <div>
+                      <a href='#'>
+                      About
+                      </a>
+                    </div>
                     {/* Contact */}
-                    <div>Contact</div>
+                    <div>
+                      <a href='#'>
+                      Contact
+                      </a>
+                    </div>
                     {/* Login */}
-                    <div>Login</div>
+                    <div>
+                      <a href='#'>
+                      Login
+                      </a>
+                    </div>
                 </div>
       </div>
-      <div className="bg-white h-0.5 w-full"></div>
-      <div className='px-10 py-4'>
-        here there will be socials
+      <hr className="border-t border-white py-1 mt-5" />
+      <div className='flex justify-between items-center pb-10'>
+      <div className='flex  gap-8 font-extralight text-sm'>
+            <div>
+              {/* copyright */}
+              Copyright @2024 ProjectWhiz
+            </div>
+            {/* privacy policy */}
+            <div>
+            <a href='#'>
+            Privacy policy
+            </a>
+            </div>
+            {/* terms */}
+            <div>
+            <a href='#'>
+            Terms
+            </a>
+            </div>
+
+      </div>
+      {/* socials */}
+      <div className='flex gap-4'>
+        {/* instagram */}
+         <a href='https://instagram.com' target='_blank' rel='noopener noreferrer'>
+            <FaInstagram style={{ fontSize: '20px' }} />
+          </a>
+        {/* facebook */}
+        <a href='https://facebook.com' target='_blank' rel='noopener noreferrer'>
+            <FaFacebook style={{ fontSize: '20px' }}  />
+          </a>
+          {/* twitter */}
+          <a href='https://twitter.com' target='_blank' rel='noopener noreferrer'>
+            <FaTwitter style={{ fontSize: '20px' }}  />
+          </a>
+          {/* linkedin */}
+          <a href='https://linkedin.com' target='_blank' rel='noopener noreferrer'>
+            <FaLinkedin style={{ fontSize: '20px' }}  />
+          </a>
+      </div>
       </div>
     </div>
   );
