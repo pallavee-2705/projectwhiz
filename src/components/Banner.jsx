9eff7a94e213894018e9c3e5f583455272ac3be3
pallavee-2705 <pallaveebisoyee@gmail.com>
2024-04-09 13:36:54 -0400

    made banner responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -16,13 +16,14 @@ function Banner() {
           Say goodbye to complex project management tools and hello to simplicity, efficiency, and success.
         </div>
         {/* Email input and Get Started button */}
-        <div className="flex flex-col w-full md:flex-row items-center justify-center md:justify-start mt-5">
-          <input type="email" placeholder="Enter your email" className="border border-gray-300 rounded-xl px-3 py-2 mb-3 md:mb-0 md:mr-3" />
+        <div className="flex flex-col md:w-full md:flex-row items-center justify-center md:justify-start mt-5">
+          <input type="email" placeholder="Enter your email" className="border border-gray-300 rounded-xl px-4 py-2 mb-3 md:mb-0 md:mr-3 w-full md:w-auto" />
           <a href="/login" className="text-white bg-gradient-to-r from-blue-500 to-blue-800 px-4 py-2 font-semibold rounded-xl flex items-center justify-center relative transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg">
             <span className="flex-shrink-0 max-lg:text-sm">Get Started</span>
             <span className="ml-2"><FaArrowRight /></span>
           </a>
         </div>
+
       </div>
       
       {/* Right section (Image) - Hidden on smaller screens */}
